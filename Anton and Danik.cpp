c4197a5580b5a743fa0f827f493b6d1b2b295f3b
HayatAbdulfetah <hayatabdulfetah7@gmail.com>
2025-02-10 12:01:44 +0300

    Create Anton and Danik.cpp

---
+++ Anton and Danik.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+    int n,a=0,d=0;
+    char str[n];
+    scanf("%d",&n);
+    scanf("%s",str);
+    for(int i=0; str[i]!='\0'; i++) {
+        if(str[i]=='A')
+            a++;
+        else
+            d++;
+ 
+    }
+    if(a==d)
+        printf("Friendship");
+    else if(a>d)
+    printf("Anton");
+    else if(d>a)
+    printf("Danik");
+    return 0;
+}
