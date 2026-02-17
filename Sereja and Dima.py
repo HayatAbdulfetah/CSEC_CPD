67f8ea5db4f684ee8ce8144397b8871b7ad7ac24
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-02-17 16:20:00 +0300

    Create Sereja and Dima.py

---
+++ Sereja and Dima.py
@@ -0,0 +1,20 @@
+n = int(input())
+cards = list(map(int, input().split()))
+
+left, right = 0, n - 1
+S, D = 0, 0
+
+for i in range(n):
+    if cards[left] > cards[right]:
+        picked = cards[left]
+        left += 1
+    else:
+        picked = cards[right]
+        right -= 1
+    
+    if i % 2 == 0:
+        S += picked
+    else:
+        D += picked
+
+print(S, D)
