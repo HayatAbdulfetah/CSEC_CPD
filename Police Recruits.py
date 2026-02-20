74ffecbea7b0d14c78d22ef6d066c11584860c34
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-02-20 12:10:54 +0300

    Create Police Recruits.py

---
+++ Police Recruits.py
@@ -0,0 +1,15 @@
+n = int(input())
+integers = [int(n) for n in input().split()]
+police = 0
+crime = 0
+
+for n in integers:
+    if n == -1:
+        if police > 0:
+            police -= 1
+        else:
+            crime += 1
+    else:
+        police += n
+        
+print(crime)
