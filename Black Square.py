d4593850b57418d99c46164a7017d37c7fbc217d
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-02-20 13:08:49 +0300

    Update Black Square.py

--- Black Square.py
+++ Black Square.py
@@ -1,4 +1,8 @@
-a=list(map(int,input().split()))
-s=input()
-total=sum(a[int(x)-1] for x in s)
-print(total)
+a = [int(i) for i in input().split()]
+s = input()
+cal = 0
+
+for i in s:
+    cal += a[int(i)-1]
+#we can use cal = sum(a[int(x)-1] for x in s) istead of using the for loop
+print(cal)
