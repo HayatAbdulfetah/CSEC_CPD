80b1d833099ac50679b06ec7702696249bb02cc6
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-07 21:51:45 +0300

    Update counting logic for odd and even numbers
    Refactor logic to count odd, zero, and two values in the list.

--- Minus Two.py
+++ Minus Two.py
@@ -1,14 +1,21 @@
-from collections import Counter
-
 t = int(input())
 
 for _ in range(t):
     n = int(input())
     a = list(map(int, input().split()))
 
-    odd = sum(x % 2 for x in a)
-    even = n - odd
+    odd = 0
+    zero = 0
+    two = 0
 
-    print(max(odd, even))
+    for x in a:
+        if x % 2 == 1:
+            odd += 1
+        elif x % 4 == 0:
+            zero += 1
+        else:
+            two += 1
+
+    print(max(odd, zero, two))
 
 # prooblem link --> https://codeforces.com/contest/2259/problem/B
