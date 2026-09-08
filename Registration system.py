2292fccd8bb1230b7cc9b0d384a207275549d32d
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-08 23:00:47 +0300

    Fix indentation and improve input handling

--- Registration system.py
+++ Registration system.py
@@ -1,2 +1,13 @@
+n = int(input())
+stack = []
+for _ in range(n):
+  s = input()
+  count = 0
+  if s in stack:
+    count += 1
+    print(s + str(count))
+  else:
+    stack.append(s)
+    print("OK")
 
 # Codeforrces problem link --> https://codeforces.com/problemset/problem/4/C
