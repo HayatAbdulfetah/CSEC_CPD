b15c4ee7d4f1895240af8850ff32d895a07816d6
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-08 23:12:40 +0300

    Update Registration system.py

--- Registration system.py
+++ Registration system.py
@@ -1,31 +1,17 @@
+another simpler solution 
 n = int(input())
-stack = []
-count = 0
+
+users = {}
+
 for _ in range(n):
-  s = input()
-  
-  if s in stack:
-    count += 1
-    print(s + str(count))
-  else:
-    stack.append(s)
-    print("OK")
-    count = 0
+    s = input()
 
-# another simpler solution 
-# n = int(input())
-
-# users = {}
-
-# for _ in range(n):
-#     s = input()
-
-#     if s not in users:
-#         users[s] = 0
-#         print("OK")
-#     else:
-#         users[s] += 1
-#         print(s + str(users[s]))
+    if s not in users:
+        users[s] = 0
+        print("OK")
+    else:
+        users[s] += 1
+        print(s + str(users[s]))
 
 
 # Codeforrces problem link --> https://codeforces.com/problemset/problem/4/C
