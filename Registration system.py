4c6ea731fa5ec921e949fbb28d5c19a177f6574b
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-08 23:05:02 +0300

    Update Registration system.py

--- Registration system.py
+++ Registration system.py
@@ -11,4 +11,20 @@ for _ in range(n):
     stack.append(s)
     print("OK")
 
+# another simpler solution 
+# n = int(input())
+
+# users = {}
+
+# for _ in range(n):
+#     s = input()
+
+#     if s not in users:
+#         users[s] = 0
+#         print("OK")
+#     else:
+#         users[s] += 1
+#         print(s + str(users[s]))
+
+
 # Codeforrces problem link --> https://codeforces.com/problemset/problem/4/C
