313daa9fabaee94fe89f71f19d105b3af673e3a8
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-10 00:29:37 +0300

    Refactor code to check if two strings are anagrams

--- Amusing Joke.py
+++ Amusing Joke.py
@@ -1,1 +1,10 @@
+a = input()
+b = input()
+c = input()
+
+if sorted(a + b) == sorted(c):
+    print("YES")
+else:
+    print("NO")
+
 # Codeforces problem link --> https://codeforces.com/problemset/problem/141/A
