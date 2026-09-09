73221785c7db9d55ef5c7aa7a275efea856b183b
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-09 23:10:06 +0300

    Add solution for New Year and Hurry problem
    Implement solution for Codeforces problem 750/A.

--- New Year and Hurry.py
+++ New Year and Hurry.py
@@ -1,1 +1,17 @@
+n, k = map(int, input().split())
+
+time = 240 - k
+total = 0
+answer = 0
+
+for i in range(1, n + 1):
+    total += 5 * i
+
+    if total <= time:
+        answer += 1
+    else:
+        break
+
+print(answer)
+
 # Codeforces problem link --> https://codeforces.com/problemset/problem/750/A
