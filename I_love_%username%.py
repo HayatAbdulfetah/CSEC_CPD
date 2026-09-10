c8dd99aa369d4e07e1a06e74e794fa9000f4168b
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-11 00:17:12 +0300

    Clean up comments in I_love_%username%.py
    Removed duplicate Codeforces problem link comment.

--- I_love_%username%.py
+++ I_love_%username%.py
@@ -1,3 +1,21 @@
+n = int(input())
+a = list(map(int, input().split()))
 
+maximum = a[0]
+minimum = a[0]
+
+count = 0
+
+for i in range(1, n):
+
+    if a[i] > maximum:
+        count += 1
+        maximum = a[i]
+
+    elif a[i] < minimum:
+        count += 1
+        minimum = a[i]
+
+print(count)
 
 # Codeforces problem link --> https://codeforces.com/problemset/problem/155/A
